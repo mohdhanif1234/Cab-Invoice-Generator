ac27f7c8da7e5d567c5706e43e0959ac993693dc
mohdhanif1234 <hanif.topia@gmail.com>
2021-10-14 17:47:11 +0530

    [Hanif]:Added a program to calculate and test average fare, total no of rides and total fare

--- MSTestForCabInvoiceGenerator/UnitTest1.cs
+++ MSTestForCabInvoiceGenerator/UnitTest1.cs
@@ -20,7 +20,7 @@ namespace MSTestForCabInvoiceGenerator
             Assert.AreEqual(expected, fare);
         }
         // UC-2: Calculate total fare for multiple rides
-        //Given number of rides should return total invoice summery
+        //Given number of rides should return total invoice summary
         [TestMethod]
         [TestCategory("Fare")]
         public void GivenMultipleRidesShouldReturnInvoiceSummary()
@@ -30,6 +30,40 @@ namespace MSTestForCabInvoiceGenerator
             InvoiceSummary actual = invoiceGenerator.CalculateFare(ride);
             InvoiceSummary expected = new InvoiceSummary(2, 60.0);
             Assert.AreEqual(expected, actual);
+        }
+        // UC-3: Given multiple rides should show total no of rides, total fare, average fare per ride
+        // UC-3: Given multiple rides should return average fare
+        [TestMethod]
+        public void ReturnAverageFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(3.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary summary = new InvoiceSummary(2, 40.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(expected.averageFare,summary.averageFare);
+        }
+        // UC:3- Given multiple rides should returns number of rides 
+        [TestMethod]
+        public void ReturnNumberofRides()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(3.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 40.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(expected.noOfRides, summary.noOfRides);
+        }
+
+        // UC-3: given multiple rides should returns Total fare
+        [TestMethod]
+        public void ReturnTotalFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(3.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 40.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(expected.totalFare, summary.totalFare);
         }
     }
 }
